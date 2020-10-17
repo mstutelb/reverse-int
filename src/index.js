828cf6f8f4923304ac026f2f6fd6e2b00037f064
Vasilkevich Ilya <Vasilkevich@redsoft.ru>
2020-10-17 18:19:33 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let operator = "";
+    if (n < 0) {
+        operator = "-";
+        n *= - 1;
+    }
+    const string = n.toString();
+    return +Array.from(string).reverse().join("");
+};
